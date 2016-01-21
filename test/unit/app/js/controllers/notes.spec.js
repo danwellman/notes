7f78a7a13e46330f0a3659f9b32ba6705bd320ed
Dan Wellman <danwellman@hotmail.com>
2016-01-21 21:10:12 +0000

    added some tests

--- test/unit/app/js/controllers/notes.spec.js
+++ test/unit/app/js/controllers/notes.spec.js
@@ -1,10 +1,68 @@
 ﻿(function () {
     'use strict';
 
-    describe('a test test', function () {
-        it('can run a test', function () {
-            expect(true).toEqual(true);
+    beforeEach(module('notes'));
+
+    describe('the Notes controller', function () {
+
+        var $scope, $document, $timeout, localStorageService, uuid, sandbox;
+
+        beforeEach(inject(function ($rootScope, _localStorageService_) {
+            localStorageService = _localStorageService_;
+
+            $scope = $rootScope.$new();
+
+            sandbox = sinon.sandbox.create();
+        }));
+
+        afterEach(function () {
+            sandbox.restore();
         });
+
+        describe('when localStorage is not supported', function () {
+
+            beforeEach(inject(function ($controller) {
+                sandbox.stub(localStorageService, 'supportsStorage').returns(false);
+
+                $controller('notesController', { $scope: $scope });
+            }));
+
+            it('sets the localStorageSupported property to false', function () {
+                expect($scope.localStorageSupported).toEqual(false);
+            });
+
+        });
+
+        describe('.hideModal()', function () {
+
+            beforeEach(inject(function ($controller) {
+                $controller('notesController', { $scope: $scope });
+            }));
+
+            describe('when the triggering element has the overlay class', function () {
+
+                it('sets the showModal property to false', function () {
+
+                    var fakeEvent = {
+                        target: {
+                            classList: {
+                                contains: function () {
+                                    return true;
+                                }
+                            }
+                        }
+                    };
+
+                    $scope.showModal = true;
+                    $scope.hideModal(fakeEvent);
+
+                    expect($scope.showModal).toEqual(false);
+                });
+
+            });
+
+        });
+
     });
 
 }());
