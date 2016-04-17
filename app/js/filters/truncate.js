c7c4337207dc967bf1368d3499b70c685a967a5e
Dan Wellman <danwellman@hotmail.com>
2016-04-17 22:03:13 +0100

    Added truncate filter for long note names

---
+++ app/js/filters/truncate.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('notes').filter('truncate', truncate);
+
+    function truncate() {
+        return function (string, length) {
+            return (string.length > length) ? string.slice(0, length - 1) + '...' : string;
+        };
+    }
+}());
