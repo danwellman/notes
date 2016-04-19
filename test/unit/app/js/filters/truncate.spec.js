9ea54fcad605489e097e367a621af98919f28b83
Dan Wellman <danwellman@hotmail.com>
2016-04-19 22:31:56 +0100

    Working tests

---
+++ test/unit/app/js/filters/truncate.spec.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    'use strict';
+
+    beforeEach(module('notes'));
+
+    describe('the Truncate filter', function () {
+
+        var truncateFilter, sandbox;
+
+        beforeEach(inject(function (_truncateFilter_) {
+            truncateFilter = _truncateFilter_;
+        }));
+
+        it('truncates a string at the specified length and adds an ellipsis', function () {
+            var testString = 'Test',
+                truncatedString = truncateFilter(testString, 1);
+
+            expect(truncatedString.length).toEqual(4);
+            expect(/\.\.\.$/.test(truncatedString)).toEqual(true);
+        });
+
+        it('does not truncate the string if less than or equal to the specified length', function () {
+            var testString = 'Test',
+                truncatedString = truncateFilter(testString, 4);
+
+            expect(/\.\.\.$/.test(truncatedString)).toEqual(false);
+        });
+
+    });
+}());
